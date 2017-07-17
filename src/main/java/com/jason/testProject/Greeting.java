d17ad096490a084daa8604732aff760088baf6bf
luwenshuo <luwenshuo@hotmail.com>
2017-07-17 21:27:31 +0800

    initial commit

---
+++ src/main/java/com/jason/testProject/Greeting.java
@@ -0,0 +1,24 @@
+package com.jason.testProject;
+
+/**
+ * Created by Jason on 17/7/17.
+ */
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
